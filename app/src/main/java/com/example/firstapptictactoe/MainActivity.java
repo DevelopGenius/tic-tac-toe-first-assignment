7cf6a1170bb51a74ee39b68761f4f51261840420
DevelopGenius <maximpidchenko@gmail.com>
2022-12-03 21:42:50 +0200

    Adding partial logic

--- app/src/main/java/com/example/firstapptictactoe/MainActivity.java
+++ app/src/main/java/com/example/firstapptictactoe/MainActivity.java
@@ -6,9 +6,47 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    State currPlayer;
+    final int arrSize = 3;
+
+    enum State {
+        EMPTY,
+        X,
+        O,
+    }
+
+    State stateArray[][] = new State[arrSize][arrSize];
+
+    int[][][] winPositions = {{{0,0}, {0,1}, {0,2}}, {{1,0}, {1,1}, {1,2}}, {{2,0}, {2,1}, {2,2}},
+                             {{0,0}, {1,0}, {2,0}}, {{0,1}, {1,1}, {2,1}}, {{0,2}, {1,2}, {2,2}},
+                             {{0,0}, {1,1}, {2,2}}, {{0,2}, {1,1}, {2,0}}};
+
+    public void onClick () {
+
+    }
+
+    boolean isWinner () {
+        for(int[][] winPosition : winPositions) {
+            if (stateArray[winPosition[0][0]][winPosition[0][1]] != State.EMPTY &&
+                    stateArray[winPosition[0][0]][winPosition[0][1]] == stateArray[winPosition[1][0]][winPosition[1][1]] &&
+                    stateArray[winPosition[1][0]][winPosition[1][1]] == stateArray[winPosition[2][0]][winPosition[2][1]]
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i = 0; i < arrSize; i++) {
+            for(int j = 0; j < arrSize; j++) {
+                stateArray[i][j] = State.EMPTY;
+            }
+        }
     }
 }
